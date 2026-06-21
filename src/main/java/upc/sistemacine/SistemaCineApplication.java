5d52e5055a9e25ce199e8762ed64fd7db171eb39
EricsonCR <ericson4634@gmail.com>
2026-06-20 19:54:17 -0500

    crear proyecto, paquetes

---
+++ src/main/java/upc/sistemacine/SistemaCineApplication.java
@@ -0,0 +1,13 @@
+package upc.sistemacine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/sistemacine/SistemaCineApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.sistemacine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
